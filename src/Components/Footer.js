4c5629dfecfcd8811c80528ac500e0a6a855fded
Eric Sellors <github@esellors.com>
2020-01-29 11:42:05 -0600

    set up front components and server

---
+++ src/Components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+    return <p>Copyright 2020</p>
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+    return <h2>My Travels</h2>
+}
+
+export default Header;
